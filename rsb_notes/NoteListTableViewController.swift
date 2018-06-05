a42272db9b51677ec53220415168a4eb4c56b399
Roman <cold.steel.br@gmail.com>
2018-06-05 18:54:16 +0700

    Empty List screen & Edit & + buttons

---
+++ rsb_notes/NoteListTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  NoteListTableViewController.swift
+//  rsb_notes
+//
+//  Created by Roman Brazhnikov on 05.06.2018.
+//  Copyright © 2018 Roman Brazhnikov. All rights reserved.
+//
+
+import UIKit
+
+class NoteListTableViewController: UITableViewController{
+    
+    
+    
+}
